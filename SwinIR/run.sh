7b34c2beb3038f553124adf4156338b514681865
YuLetian <yu.letian66@gmail.com>
2025-07-20 16:51:20 +0800

    clean swinir

---
+++ SwinIR/run.sh
@@ -0,0 +1,5 @@
+python main_test_swinir.py \
+  --task color_dn \
+  --noise 15 \
+  --model_path model_zoo/swinir/005_colorDN_DFWB_s128w8_SwinIR-M_noise15.pth \
+  --folder_gt ../GTSRB/train/Final_Training/Images/00000

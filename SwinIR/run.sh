a3f13946b6332ce94230a86824f6fc6637e7549a
YuLetian <yu.letian66@gmail.com>
2025-07-21 10:36:52 +0800

    provide noise_type choice

--- SwinIR/run.sh
+++ SwinIR/run.sh
@@ -1,5 +1,6 @@
 python main_test_swinir.py \
   --task color_dn \
+  --noise_type 'gaussian' \
   --noise 15 \
   --model_path model_zoo/swinir/005_colorDN_DFWB_s128w8_SwinIR-M_noise15.pth \
   --folder_gt ../GTSRB/train/Final_Training/Images/00000

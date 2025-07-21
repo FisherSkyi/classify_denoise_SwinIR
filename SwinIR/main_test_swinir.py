8cf3c482dd9b83e8eda9c4d8200180f3007af7d1
YuLetian <yu.letian66@gmail.com>
2025-07-21 10:44:47 +0800

    remove unnecessary branch

--- SwinIR/main_test_swinir.py
+++ SwinIR/main_test_swinir.py
@@ -160,38 +160,8 @@ def main():
 
 
 def define_model(args):
-    # 001 classical image sr
-    if args.task == 'classical_sr':
-        model = net(upscale=args.scale, in_chans=3, img_size=args.training_patch_size, window_size=8,
-                    img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
-                    mlp_ratio=2, upsampler='pixelshuffle', resi_connection='1conv')
-        param_key_g = 'params'
-
-    # 002 lightweight image sr
-    # use 'pixelshuffledirect' to save parameters
-    elif args.task == 'lightweight_sr':
-        model = net(upscale=args.scale, in_chans=3, img_size=64, window_size=8,
-                    img_range=1., depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6],
-                    mlp_ratio=2, upsampler='pixelshuffledirect', resi_connection='1conv')
-        param_key_g = 'params'
-
-    # 003 real-world image sr
-    elif args.task == 'real_sr':
-        if not args.large_model:
-            # use 'nearest+conv' to avoid block artifacts
-            model = net(upscale=args.scale, in_chans=3, img_size=64, window_size=8,
-                        img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
-                        mlp_ratio=2, upsampler='nearest+conv', resi_connection='1conv')
-        else:
-            # larger model size; use '3conv' to save parameters and memory; use ema for GAN training
-            model = net(upscale=args.scale, in_chans=3, img_size=64, window_size=8,
-                        img_range=1., depths=[6, 6, 6, 6, 6, 6, 6, 6, 6], embed_dim=240,
-                        num_heads=[8, 8, 8, 8, 8, 8, 8, 8, 8],
-                        mlp_ratio=2, upsampler='nearest+conv', resi_connection='3conv')
-        param_key_g = 'params_ema'
-
     # 004 grayscale image denoising
-    elif args.task == 'gray_dn':
+    if args.task == 'gray_dn':
         model = net(upscale=1, in_chans=1, img_size=128, window_size=8,
                     img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
                     mlp_ratio=2, upsampler='', resi_connection='1conv')
@@ -201,22 +171,6 @@ def define_model(args):
     elif args.task == 'color_dn':
         model = net(upscale=1, in_chans=3, img_size=128, window_size=8,
                     img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
-                    mlp_ratio=2, upsampler='', resi_connection='1conv')
-        param_key_g = 'params'
-
-    # 006 grayscale JPEG compression artifact reduction
-    # use window_size=7 because JPEG encoding uses 8x8; use img_range=255 because it's sligtly better than 1
-    elif args.task == 'jpeg_car':
-        model = net(upscale=1, in_chans=1, img_size=126, window_size=7,
-                    img_range=255., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
-                    mlp_ratio=2, upsampler='', resi_connection='1conv')
-        param_key_g = 'params'
-
-    # 006 color JPEG compression artifact reduction
-    # use window_size=7 because JPEG encoding uses 8x8; use img_range=255 because it's sligtly better than 1
-    elif args.task == 'color_jpeg_car':
-        model = net(upscale=1, in_chans=3, img_size=126, window_size=7,
-                    img_range=255., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
                     mlp_ratio=2, upsampler='', resi_connection='1conv')
         param_key_g = 'params'
 
@@ -230,34 +184,13 @@ def define_model(args):
 def setup(args):
     timestamp = time.strftime('%Y%m%d-%H%M%S')
     # 001 classical image sr/ 002 lightweight image sr
-    if args.task in ['classical_sr', 'lightweight_sr']:
-        save_dir = f'results/swinir_{args.task}_x{args.scale}_{timestamp}'
-        folder = args.folder_gt
-        border = args.scale
-        window_size = 8
-
-    # 003 real-world image sr
-    elif args.task in ['real_sr']:
-        save_dir = f'results/swinir_{args.task}_x{args.scale}_{timestamp}'
-        if args.large_model:
-            save_dir += '_large'
-        folder = args.folder_lq
-        border = 0
-        window_size = 8
 
     # 004 grayscale image denoising/ 005 color image denoising
-    elif args.task in ['gray_dn', 'color_dn']:
+    if args.task in ['gray_dn', 'color_dn']:
         save_dir = f'results/swinir_{args.task}_noise{args.noise}_type{args.noise_type}_{timestamp}'
         folder = args.folder_gt
         border = 0
         window_size = 8
-
-    # 006 JPEG compression artifact reduction
-    elif args.task in ['jpeg_car', 'color_jpeg_car']:
-        save_dir = f'results/swinir_{args.task}_jpeg{args.jpeg}_{timestamp}'
-        folder = args.folder_gt
-        border = 0
-        window_size = 7
 
     return folder, save_dir, border, window_size
 
@@ -265,19 +198,9 @@ def setup(args):
 def get_image_pair(args, path):
     (imgname, imgext) = os.path.splitext(os.path.basename(path))
 
-    # 001 classical image sr/ 002 lightweight image sr (load lq-gt image pairs)
-    if args.task in ['classical_sr', 'lightweight_sr']:
-        img_gt = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
-        img_lq = cv2.imread(f'{args.folder_lq}/{imgname}x{args.scale}{imgext}', cv2.IMREAD_COLOR).astype(
-            np.float32) / 255.
-
-    # 003 real-world image sr (load lq image only)
-    elif args.task in ['real_sr']:
-        img_gt = None
-        img_lq = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
 
     # 004 grayscale image denoising (load gt image and generate lq image on-the-fly)
-    elif args.task in ['gray_dn']:
+    if args.task in ['gray_dn']:
         img_gt = cv2.imread(path, cv2.IMREAD_GRAYSCALE).astype(np.float32) / 255.
         np.random.seed(seed=0)
         img_lq = img_gt + np.random.normal(0, args.noise / 255., img_gt.shape)
@@ -291,24 +214,6 @@ def get_image_pair(args, path):
             img_lq = img_gt + np.random.normal(0, args.noise / 255., img_gt.shape)
         elif args.noise_type == 'graffiti':
             img_lq = np.array(addshapeall.add_random_shapes(img_gt, 0.3)).astype(np.float32) / 255.
-
-    # 006 grayscale JPEG compression artifact reduction (load gt image and generate lq image on-the-fly)
-    elif args.task in ['jpeg_car']:
-        img_gt = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-        if img_gt.ndim != 2:
-            img_gt = util.bgr2ycbcr(img_gt, y_only=True)
-        result, encimg = cv2.imencode('.jpg', img_gt, [int(cv2.IMWRITE_JPEG_QUALITY), args.jpeg])
-        img_lq = cv2.imdecode(encimg, 0)
-        img_gt = np.expand_dims(img_gt, axis=2).astype(np.float32) / 255.
-        img_lq = np.expand_dims(img_lq, axis=2).astype(np.float32) / 255.
-
-    # 006 JPEG compression artifact reduction (load gt image and generate lq image on-the-fly)
-    elif args.task in ['color_jpeg_car']:
-        img_gt = cv2.imread(path)
-        result, encimg = cv2.imencode('.jpg', img_gt, [int(cv2.IMWRITE_JPEG_QUALITY), args.jpeg])
-        img_lq = cv2.imdecode(encimg, 1)
-        img_gt = img_gt.astype(np.float32)/ 255.
-        img_lq = img_lq.astype(np.float32)/ 255.
 
     return imgname, img_lq, img_gt
 
